f566dbe9bace0bf589841e7ff8851860faa84663
Deepak Kumar <dk11knight@gmail.com>
2024-05-05 00:25:21 +0530

    dotenv, whitenoise removed

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -13,9 +13,6 @@ import os
 from pathlib import Path
 import dj_database_url
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'CMR_Attendance_Module.urls'
@@ -124,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static' #specify the directory where the collectstatic command will collect all the static files 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #dependency whitenoise
-# used to specify additional directories from which to load static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -138,3 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 AUTH_USER_MODEL = 'app.CustomUser'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
