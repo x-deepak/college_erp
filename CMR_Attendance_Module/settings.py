7e01a61fa1857d86073ae41317ddbd773abe11db
Deepak Kumar <dk11knight@gmail.com>
2024-05-04 22:10:37 +0530

    postgres added and tested, - is working but data left to add

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,6 +82,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.parse('postgres://default:7QydroUIM8ns@ep-misty-wind-a1t9azl3.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
