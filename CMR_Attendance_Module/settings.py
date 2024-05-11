a7ff84f886963cdac3f42d736fe18eebd88a2ce5
Deepak Kumar <dk11knight@gmail.com>
2024-05-11 21:37:08 +0530

    Stopped tracking .env File

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEYY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app' ]
 
 
 # Application definition
