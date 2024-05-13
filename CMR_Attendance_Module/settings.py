94e7ef0a6881689e65ba527b200222d3d93d9bc8
Deepak Kumar <dk11knight@gmail.com>
2024-05-13 21:26:10 +0530

    minor changesss

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
